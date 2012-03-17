34e4dda71c73404a6efef10b99cb1fb03e314eea
Rıdvan Örsvuran <flasherdn@gmail.com>
2012-03-17 17:41:38 +0200

    use argparse

--- extract.py
+++ extract.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
+import argparse
 import datetime
 import json
 
 from kandillibulten.database import MongoDB
 from kandillibulten.parsers import PdfParser
-
-
-from optparse import OptionParser
 
 def data2json(data, jsonfilename):
     is_date = lambda x: isinstance(x, datetime.datetime)
@@ -20,36 +18,39 @@ def data2mongo(data, dbname):
     db.insert(data)
 
 if __name__ == '__main__':
-    parser = OptionParser(usage="usage: %prog [options] filetoparse.pdf")
-    parser.add_option("-s", "--start-page", dest="startpage", default=1,
-                      type="int",
-                      help="First page to parse", metavar="PAGE_NUM")
-    parser.add_option("-e", "--end-page", dest="endpage", default=None,
-                      help="Last page to parse", metavar="PAGE_NUM")
-    parser.add_option("-p", "--print",
-                      action="store_true", dest="printdata", default=False,
-                      help="Print parsed data")
-    parser.add_option("-t", "--timeit",
+    parser = argparse.ArgumentParser(description="Extract data from pdf file")
+    parser.add_argument("filetoparse", type=str,
+                        help="Pdf file to parse")
+    parser.add_argument("-s", "--start-page", dest="startpage", default=1,
+                        type=int, help="First page to parse",
+                        metavar="PAGE_NUM")
+    parser.add_argument("-e", "--end-page", dest="endpage", default=None,
+                        type=int, help="Last page to parse",
+                        metavar="PAGE_NUM")
+    parser.add_argument("-p", "--print",
+                        action="store_true", dest="printdata",
+                        default=False, help="Print parsed data")
+    parser.add_argument("-t", "--timeit",
                       action="store_true", dest="timeit", default=False,
                       help="Calc parsing time")
-    parser.add_option("-j", "--dump-json", type="string", dest="json",
+    parser.add_argument("-j", "--dump-json", type=str, dest="json",
                       default=None, metavar="FILENAME",
                       help="Dump data to json file")
-    parser.add_option("-m", "--dump-mongodb", type="string", dest="mongo",
+    parser.add_argument("-m", "--dump-mongodb", type=str, dest="mongo",
                       default=None, metavar="DB_NAME",
                       help="Dump data to mongodb")
-    (options, args) = parser.parse_args()
-    if len(args) >= 1:
-        pdf_data = PdfParser(args[0], options.startpage, options.endpage)
-        if options.timeit:
-            import time
-            start = time.time()
-            pdf_data.parse_data()
-            print "Data parsed in %.2f seconds." % (time.time() - start)
-        if options.json:
-            data2json(pdf_data.parse_data(), options.json)
-        if options.mongo:
-            data2mongo(pdf_data.parse_data(), options.mongo)
-        if options.printdata:
-            pdf_data.parse_data()
-            pdf_data.print_data()
+    args = parser.parse_args()
+    pdf_data = PdfParser(args.filetoparse,
+                         args.startpage, args.endpage)
+    if args.timeit:
+        import time
+        start = time.time()
+        pdf_data.parse_data()
+        print "Data parsed in %.2f seconds." % (time.time() - start)
+    if args.json:
+        data2json(pdf_data.parse_data(), args.json)
+    if args.mongo:
+        data2mongo(pdf_data.parse_data(), args.mongo)
+    if args.printdata:
+        pdf_data.parse_data()
+        pdf_data.print_data()
